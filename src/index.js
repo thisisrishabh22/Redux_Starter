0ca632b4200583eb1c660c29d8aefdadae45039b
Rishabh Singh <ris8645208@gmail.com>
2021-09-07 21:32:01 +0530

    chore: add updating immutable objects

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-const person = { name: "John" };
+const person = {
+  name: "John",
+  address: {
+    country: "USA",
+    city: "California",
+  },
+};
 console.log(person);
-const newPerson = Object.assign({}, person, { name: "Bob", age: 30 });
+const newPerson = Object.assign({ ...person, name: "Bob" });
+// newPerson.address.city = "New York"; // Original property will also be changed 
 console.log(newPerson);
