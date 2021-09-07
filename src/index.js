0439697d4a319b7b71857e474e4966cff5764a4b
Rishabh Singh <ris8645208@gmail.com>
2021-09-07 15:56:59 +0530

    chore: add loadlash to main

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+import { compose, pipe } from "lodash/fp";
+
 let input = " Javascript           ";
 let output = "<div>" + input.trim() + "</div>";
 
 const trim = (str) => str.trim();
-
 const renderOutput = (str) => `<div>${str}</div>`;
+const lowerCase = (str) => str.toLowerCase();
 
-console.log(renderOutput(trim(input)));
+const transform = pipe(trim, lowerCase, renderOutput);
+
+console.log(transform(input));
+
+console.log(renderOutput(lowerCase(trim(input))));
