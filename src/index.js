006575a50ff9c06e10fc463abc2fabec4effcb4e
Rishabh Singh <ris8645208@gmail.com>
2021-09-07 21:00:26 +0530

    chore: add basic immutable object

--- src/index.js
+++ src/index.js
@@ -1,18 +1,4 @@
-import { compose, pipe } from "lodash/fp";
-import { add } from "./currying";
-
-let input = " Javascript           ";
-let output = "<div>" + input.trim() + "</div>";
-
-const trim = (str) => str.trim();
-const wrap = (type) => (str) => `<${type}>${str}</${type}>`;
-
-const lowerCase = (str) => str.toLowerCase();
-
-const transform = pipe(trim, lowerCase, wrap("div"));
-
-console.log(transform(input));
-
-// console.log(wrap(lowerCase(trim(input))));
-
-console.log(add(1)(2));
+const person = { name: "John" };
+console.log(person);
+const newPerson = Object.assign({}, person, { name: "Bob", age: 30 });
+console.log(newPerson);
