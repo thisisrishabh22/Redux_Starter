0f9e8a14eaf3799bc4678ebc94c4be90b7cf8b63
Rishabh Singh <ris8645208@gmail.com>
2021-09-07 13:26:19 +0530

    chore: update proper functional composition

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-let input = " Javascript ";
+let input = " Javascript           ";
 let output = "<div>" + input.trim() + "</div>";
 
 const trim = (str) => str.trim();
 
 const renderOutput = (str) => `<div>${str}</div>`;
 
-console.log(renderOutput("Javascript"));
+console.log(renderOutput(trim(input)));
