6fae2a67437458eb5b1b0f39305a2d4ec2a2fb61
Rishabh Singh <ris8645208@gmail.com>
2021-09-08 19:39:54 +0530

    refactor: replaced previous entry point

---
+++ src/main.js
@@ -0,0 +1,62 @@
+import { produce } from "immer";
+const { Map } = require("immutable");
+const { indexOf } = require("lodash");
+
+const person = {
+  name: "John",
+  address: {
+    country: "USA",
+    city: "California",
+  },
+};
+console.log(person);
+const newPerson = Object.assign({
+  ...person,
+  name: "Bob",
+  address: { ...person.address, city: "New York" }, // spreading the data from address in person assigning new value to keep immutability
+});
+// newPerson.address.city = "New York"; // Original property will also be changed
+console.log(newPerson);
+
+const numbers = [1, 2, 3];
+
+// Adding
+const indexOfNumbers = numbers.indexOf(2);
+const newNumbers = [
+  ...numbers.slice(0, indexOfNumbers),
+  4,
+  ...numbers.slice(indexOfNumbers),
+];
+
+// Removing
+const removed = numbers.filter((n) => n != 2);
+
+const updated = newNumbers.map((n) => (n == 2 ? 20 : n));
+
+console.log(newNumbers);
+console.log(removed);
+console.log(updated);
+
+// Immutable Js
+let book = Map({ title: "Harry Porter" });
+
+function publish(book) {
+  return book.set("isPublished", true);
+}
+
+book = publish(book);
+
+console.log(book.toJS());
+
+// Immer Js
+let immerBook = { title: "Harry Porter" };
+
+function immerPublish(book) {
+  return produce(book, (draftBook) => {
+    draftBook.isPublished = true;
+  });
+}
+
+let updatedImmutable = immerPublish(immerBook);
+
+console.log(updatedImmutable);
