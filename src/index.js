accbfe0c555c0ba0b171890486504f88bd205f92
Rishabh Singh <ris8645208@gmail.com>
2021-09-08 11:48:45 +0530

    chore: add changing data to keep immutable object

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const person = {
   },
 };
 console.log(person);
-const newPerson = Object.assign({ ...person, name: "Bob" });
-// newPerson.address.city = "New York"; // Original property will also be changed 
+const newPerson = Object.assign({
+  ...person,
+  name: "Bob",
+  address: { ...person.address, city: "New York" }, // spreading the data from address in person assigning new value to keep immutability
+});
+// newPerson.address.city = "New York"; // Original property will also be changed
 console.log(newPerson);
